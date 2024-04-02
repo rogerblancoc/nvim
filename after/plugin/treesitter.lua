3ee762b65b5b6679270f81b625a966267f238ef5
Roger Blanco <rogerblanco10@gmail.com>
2024-04-02 20:50:00 +0200

    Disable treesitter with latex
    Due to a conflict with vimtex and treesitter, it is advised to disable
    treesitter parsing and use vimtex

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,6 +2,9 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
+  -- List of languages that will be disabled
+  ignore_install = { "latex" },
+
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
